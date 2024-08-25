489c69c00b3c21c76e63949795cc8f5a90270822
João Vítor Almeida <42156083+dunkexpial@users.noreply.github.com>
2024-08-25 19:26:45 -0300

    Projectile Mechanic Update 1

--- My project/Assets/PlayerMovement.cs
+++ My project/Assets/PlayerMovement.cs
@@ -1,16 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.EventSystems;
+using UnityEngine.InputSystem;
 
 public class playermovement : MonoBehaviour
 {
-
     public float moveSpeed;
     public Rigidbody2D rb;
     public Animator animator;
 
-    Vector2 moveDirection;
+    private Vector2 moveDirection;
+    private Vector2 lastDirection; // Track the last direction the player moved
 
     // Update is called once per frame
     void Update()
@@ -28,14 +28,32 @@ public class playermovement : MonoBehaviour
         float moveX = Input.GetAxisRaw("Horizontal");
         float moveY = Input.GetAxisRaw("Vertical");
 
+        moveDirection = new Vector2(moveX, moveY).normalized;
+
+        if (moveDirection != Vector2.zero)
+        {
+            lastDirection = moveDirection; // Update last direction when moving
+        }
+
         animator.SetFloat("Horizontal", moveDirection.x);
         animator.SetFloat("Vertical", moveDirection.y);
         animator.SetFloat("Speed", moveDirection.sqrMagnitude);
-        moveDirection = new Vector2(moveX, moveY).normalized;
+        animator.SetFloat("Horizontal", lastDirection.x);
+        animator.SetFloat("Vertical", lastDirection.y);
     }
 
-    void  Move()
+    void Move()
     {
         rb.velocity = new Vector2(moveDirection.x * moveSpeed, moveDirection.y * moveSpeed);
+    }
+
+    public Vector2 GetMoveDirection()
+    {
+        return moveDirection;
+    }
+
+    public Vector2 GetLastDirection()
+    {
+        return lastDirection;
     }
 }
